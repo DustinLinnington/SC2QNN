5b88ba7599431875292d3d9565de5287a59702c5
Dustin! <dustin.linnington@gmail.com>
2019-08-23 18:59:35 -0700

    Fixed issue with sigmoid function miscalculating

---
+++ NeuralNetwork.py
@@ -0,0 +1,205 @@
+# BUGS: 
+# 1) Always coming out as 0.5 for prediction? Training not working
+
+
+import numpy as np
+import json
+from enum import Enum
+
+class Math:
+	class ActivationFunction(Enum):
+		RELU = 1
+		LEAKY_RELU = 2
+		SIGMOID = 3
+		TANH = 4
+
+	@staticmethod
+	def calc_Z(weights, bias, prevActivation):
+		Z = np.dot(weights, prevActivation) + bias
+		return Z
+
+	@staticmethod
+	def calc_A(Z, activationFunction, dA = 0, takeDerivative = False):
+		A = Z
+		if (Math.ActivationFunction(activationFunction) == Math.ActivationFunction.RELU):
+			A = Math.calc_RELU(Z, dA, takeDerivative)
+		elif(Math.ActivationFunction(activationFunction) == Math.ActivationFunction.SIGMOID):
+			A = Math.calc_sigmoid(Z, dA, takeDerivative)
+		elif(Math.ActivationFunction(activationFunction) == Math.ActivationFunction.TANH):
+			A = Math.calc_tanH(Z, dA, takeDerivative)
+		return A
+
+	@staticmethod
+	def calc_sigmoid(Z, dA = 0, takeDerivative = False):
+		sigmoid = 1/(1 + np.exp(-Z))
+		if (takeDerivative == True):
+			return dA * sigmoid * (1 - sigmoid)
+		else:
+			return sigmoid
+
+	@staticmethod
+	def calc_tanH(Z, dA = 0, takeDerivative = False):
+		tanh = (np.exp(Z) - np.exp(-Z)) / (np.exp(Z) + np.exp(-Z))
+		if (takeDerivative == True):
+			return dA * (1 - tanh ** 2)
+		else:
+			return tanh
+
+	@staticmethod
+	def calc_RELU(Z, dA = 0, takeDerivative = False):
+		relu = np.maximum(Z, 0)
+		if (takeDerivative == True):
+			# Ensure dZ is a correct object
+			dZ = np.array(dA, copy = True)
+			# When Z <= 0, dZ should also be 0
+			dZ[Z <= 0] = 0
+			return dZ
+		else:
+			return relu
+
+	@staticmethod
+	def calc_cost(trueOutputData, calculatedOutputData):
+		m = trueOutputData.shape[1]
+		Y = trueOutputData
+		A = calculatedOutputData
+
+		cost = -np.sum(np.multiply(Y, np.log(A)) + np.multiply((1 - Y), np.log(1-A))) / m
+
+		# Fixes dimensions - turns [[20]] into 20
+		cost = np.squeeze(cost)
+		return cost
+
+class NeuralNetwork:
+	_networkData = 0
+	_networkLayers = 0
+
+	def __init__(self, networkLayers):
+		self._networkData = 0
+		self._networkLayers = networkLayers
+
+	@staticmethod
+	def load_network(fileName, networkLayers = 0):
+		jsonData = ""
+		with open(fileName, "r") as readFile:
+			jsonData = json.load(readFile)
+
+		networkLayers = jsonData["NetworkLayers"]
+		neuralNet = NeuralNetwork(networkLayers)
+		del jsonData["NetworkLayers"]
+
+		for obj in jsonData:
+			if isinstance(jsonData[obj], list):
+				jsonData[obj] = np.array(jsonData[obj])
+
+		neuralNet._networkData = jsonData
+		return neuralNet
+
+	def save_network(self, fileName):
+		jsonData = self._networkData
+		jsonData["NetworkLayers"] = self._networkLayers
+		# Convert all our numpy arrays into lists so it can be JSONified
+		for obj in jsonData:
+			if isinstance(jsonData[obj], np.ndarray):
+				jsonData[obj] = jsonData[obj].tolist()
+
+		with open(fileName, "w") as writeFile:
+			json.dump(jsonData, writeFile)
+			print("Neural Network data saved to " + fileName)
+
+	def initialize_network(self):
+		"""
+		Arguments:
+		_networkLayers -- python array (list) containing the dimensions of each layer in our network
+	
+		Returns:
+		_networkData -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+			Wl -- weight matrix of shape (_networkLayers[l], _networkLayers[l-1])
+			bl -- bias vector of shape (_networkLayers[l], 1)
+		"""
+
+		_networkData = {}
+
+		# Number of layers in the network
+		L = len(self._networkLayers)
+
+		for l in range(1, L):
+			_networkData["W" + str(l)] = np.random.randn(self._networkLayers[l], self._networkLayers[l - 1]) / np.sqrt(self._networkLayers[l-1])
+			_networkData["b" + str(l)] = np.zeros((self._networkLayers[l], 1))
+
+			assert(_networkData['W' + str(l)].shape == (self._networkLayers[l], self._networkLayers[l-1]))
+			assert(_networkData['b' + str(l)].shape == (self._networkLayers[l], 1))
+
+		return _networkData
+
+	def forward_propagate(self):
+		networkData = self._networkData
+
+		for l in range(1, len(self._networkLayers)):
+			networkData["Z" + str(l)] = Math.calc_Z(self._networkData["W" + str(l)], self._networkData["b" + str(l)], self._networkData["A" + str(l - 1)])
+
+			# Activation function of the last layer is sigmoid so we can get a value from 0 to 1
+			if (l == len(self._networkLayers) - 1):
+				networkData["A" + str(l)] = Math.calc_A(self._networkData["Z" + str(l)], Math.ActivationFunction.SIGMOID.value)
+				networkData["Z" + str(l) + ":activation"] = Math.ActivationFunction.SIGMOID.value
+			else:
+				networkData["A" + str(l)] = Math.calc_A(self._networkData["Z" + str(l)], Math.ActivationFunction.RELU.value)
+				networkData["Z" + str(l) + ":activation"] = Math.ActivationFunction.RELU.value
+
+		return networkData
+
+	# Calculate gradients for each layer of the Neural Net by using derivatives
+	def backward_propagate(self, trueOutputData):
+		numberOfTrainingExamples = trueOutputData.shape[0]
+
+		# Calculate derivatives from the end of the neural net to the beginning, using chain rule
+		gradients = {}
+		m = len(self._networkLayers)
+		
+		# Take the derivitave of the loss function (d of a of the last layer)
+		AL = self._networkData["A" + str(m - 1)]
+		gradients["dA" + str(m - 1)] = -(np.divide(trueOutputData, AL) - np.divide(1 - trueOutputData, 1 - AL))
+		
+		# Go through the layers one-by-one starting from the second last layer
+		for l in reversed(range(1, m)):
+			prevLayerSize = self._networkData["A" + str(l-1)].shape[1]
+			gradients["dZ" + str(l)] = Math.calc_A(self._networkData["Z" + str(l)], self._networkData["Z" + str(l) + ":activation"], gradients["dA" + str(l)], True)
+			gradients["dW" + str(l)] = np.dot(gradients["dZ" + str(l)], self._networkData["A" + str(l-1)].T) / prevLayerSize
+			gradients["db" + str(l)] = np.sum(gradients["dZ" + str(l)], axis = 1, keepdims = True) / prevLayerSize
+			gradients["dA" + str(l - 1)] = np.dot(self._networkData["W" + str(l)].T, gradients["dZ" + str(l)])
+			
+		return gradients
+
+	def train_network(self, inputData, trueOutputData, learningRate, iterations):
+		# Sanity check
+		assert(type(inputData).__module__ == np.__name__), "Input Data needs to be a numpy array."
+		assert(type(trueOutputData).__module__ == np.__name__), "Real Output Data needs to be a numpy array."
+		assert(np.shape(inputData)[0] == self._networkLayers[0]), "Input Data (" + str(np.shape(inputData)[0]) + ") mismatch with first layer size (" + str(self._networkLayers[0]) + ")."
+		assert(np.shape(trueOutputData)[0] == self._networkLayers[len(self._networkLayers)-1]), "Real Output Data mismatch with last layer size."
+		assert(np.shape(trueOutputData)[1] == np.shape(inputData)[1]), "Amount of Real Output Data and Input Data examples mismatch."
+		
+		# If network has never been ran before, initialize weight and bias vectors to random/zero values
+		if (self._networkData == 0):
+			self._networkData = self.initialize_network()
+		self._networkData["A0"] = inputData
+
+		for i in range(iterations):
+			self._networkData = self.forward_propagate()
+			self._networkData.update(self.backward_propagate(trueOutputData))
+			for l in range(1, len(self._networkLayers)):
+				self._networkData["W" + str(l)] = self._networkData["W" + str(l)] - learningRate * self._networkData["dW" + str(l)] 
+				self._networkData["b" + str(l)] = self._networkData["b" + str(l)] - learningRate * self._networkData["db" + str(l)]
+
+			if (i % 500 == 0):
+				cost = Math.calc_cost(trueOutputData, self._networkData["A" + str(len(self._networkLayers) - 1)])
+				print("Cost at Iteration " +  str(i) + ":", cost)
+
+	def predict(self, inputData):
+		# Ensure we're getting the right type and amount of inputs to check
+		assert (type(inputData).__module__ == np.__name__), "Input Data needs to be a numpy array."
+		assert (np.shape(inputData)[0] == self._networkLayers[0]), "Input Data mismatch with first layer size."
+		
+		self._networkData["A0"] = inputData
+		networkData = self.forward_propagate()
+		predictedOutput = networkData["A" + str(len(self._networkLayers) - 1)]
+		np.squeeze(predictedOutput)
+		return predictedOutput
---
+++ readme.txt
@@ -0,0 +1,68 @@
+/************ NEURAL NETWORK ************\
+-         Created May 07th, 2019		 -
+-         Created By: Dustin Daon        -
+-     Contact: Dustin.Daon@gmail.com     -
+------------------------------------------
+
+USE:
+=================================================================
+Create a new NeuralNetwork object and give it the parameter of an
+array describing the layers in the network and the amount of 
+neurons per layer. Ex: A network with 3 hidden layers containing 
+4 neurons per layer, 1 output layer with 1 neuron and 5 inputs
+would look like: [5, 4, 4, 4, 1]
+
+To train it, it only requires that you give it:
+1) a numpy array of n x m dimensions where n = number of inputs and 
+m = training examples,
+2) a numpy array containing the results of the training examples
+(with a shape of [output neurons, training examples])
+3) a learing rate. The higher the number the 'faster' it learns,
+but too high of a number and it can overshoot the correctness of
+its predictions which would result in a poorly trained network
+(a good value for this would be 0.0075 - 0.09)
+4) number of training iterations. The higher the number, the better
+it will learning your training set. However, this will take more
+time and you run the risk of it overfitting the training set if
+this number is too high.
+
+After training, you can save the neural network to a JSON file
+to be loaded again in the future. You can also predict a result
+from a trained network by feeding it some input data.
+
+=================================================================
+EXAMPLE
+=================================================================
+
+# Array of 3 hidden layers (size 3, 2, 2), 1 output layer (size 1)
+# and 5 input values
+networkLayers = numpy.array([5, 3, 2, 2, 1])
+
+# Input data is 10 training examples with 5 values each
+inputData = numpy.array([[1, 0, 1, 4, 5, 0, 6, 9, 1, 0], 
+ 					[4, 0, 2, 2, 3, 7, 0, 1, 4, 8], 
+ 					[6, 1, 5, 6, 5, 6, 3, 0, 1, 0], 
+ 					[0, 1, 0, 3, 4, 7, 6, 6, 6, 4], 
+ 					[8, 0, 7, 4, 3, 7, 0, 0, 1, 4]])
+
+# Output data is the correct result of the input data with shape 
+# of 1x10 (size of output layer x number of training examples)
+outputData = numpy.array([[1, 0, 0, 1, 0, 0, 0, 1, 1, 1]])
+
+# Create a new neural network of shape networkLayers
+neuralNet = NeuralNetwork(networkLayers)
+
+# Train our new neural network 1000 times with a learning rate
+# of 0.0075
+neuralNet.train_network(inputData, outputData, 0.0075, 1000)
+
+# Predict an outcome from our trained network using 1 test example
+testData = numpy.array([[1], [6], [8], [8], [9]])
+prediction = neuralNet.predict(testData)
+print(prediction)
+
+# Save our trained network
+neuralNet.save_network("neural_net.txt")
+
+# Load a different network
+starcraftNet = neuralNet.load_network("sc_neural_net.txt")
